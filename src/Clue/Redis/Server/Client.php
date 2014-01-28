83a42b95456cdca59fd944073e99b54fc9a2b165
Christian Lück <christian@lueck.tv>
2014-01-29 00:46:33 +0100

    Additional meta information for Client

--- src/Clue/Redis/Server/Client.php
+++ src/Clue/Redis/Server/Client.php
@@ -11,17 +11,26 @@ class Client
     private $connection;
     private $business;
     private $database;
+    private $name = null;
+    private $timeConnect;
+    private $timeLast;
+    private $lastRequest = null;
+    private $multi = -1;
+    private $blocking = false;
+    private $watchTouched = false;
 
     public function __construct(Connection $connection, Invoker $business, Storage $database)
     {
         $this->connection = $connection;
         $this->business = $business;
         $this->database = $database;
+        
+        $this->timeConnect = $this->timeLast = microtime(true);
     }
 
     public function getRemoteAddress()
     {
-        return $this->connection->getRemoteAddress();
+        return $this->connection->getRemoteAddress() . ':0';
     }
 
     public function close()
@@ -50,11 +59,99 @@ class Client
         return $ret;
     }
 
+    private function getFlags()
+    {
+        $flags = '';
+
+        if ($this->multi !== -1) {
+            $flags .= 'x';
+        }
+
+        if ($this->blocking) {
+            $flags .= 'b';
+        }
+
+        if ($this->watchTouched) {
+            $flags .= 'd';
+        }
+
+        if($this->isEnding()) {
+            $flags .= 'c';
+        }
+
+        if ($flags === '') {
+            $flags = 'N';
+        }
+
+        return $flags;
+    }
+
+    public function getMeta()
+    {
+        // addr=127.0.0.1:38468 fd=5 name= age=2748 idle=0 flags=N db=0 sub=0 psub=0 multi=-1 qbuf=0 qbuf-free=32768 obl=0 oll=0 omem=0 events=r cmd=client
+
+        $command = 'NULL';
+        if ($this->lastRequest !== null) {
+            $command = strtolower($lastRequest->getCommand());
+        }
+
+        $events = '';
+        if (!$this->isEnding()) {
+            $events = 'r';
+        }
+        if ($this->isWriting()) {
+            $eventts .= 'w';
+        }
+
+        return array(
+            'addr'      => $this->getRemoteAddress(),
+            'fd'        => (int)$this->connection->stream,
+            'name'      => $this->name,
+            'age'       => (int)(microtime(true) - $this->timeConnect),
+            'idle'      => (int)(microtime(true) - $this->timeLast),
+            'flags'     => $this->getFlags(),
+            'db'        => $this->database,
+            'sub'       => 0,
+            'psub'      => 0,
+            'multi'     => $this->multi,
+            'qbuf'      => 0,
+            'qbuf-free' => 0,
+            'obl'       => 0,
+            'oll'       => 0,
+            'omem'      => 0,
+            'events'    => $events,
+            'cmd'       => $command,
+        );
+    }
+
+    public function getDescription()
+    {
+        $ret = '';
+        foreach ($this->getMeta() as $name => $value) {
+            $ret .= $name . '=' . $value . ' ';
+        }
+
+        return substr($ret, 0, -1);
+    }
+
     public function handleRequest(Request $request)
     {
+        $this->lastRequest = $request;
+        $this->timeLast = microtime(true);
+
         $ret = $this->business->invoke($request);
         if ($ret !== null) {
             $this->write($ret);
         }
+    }
+
+    private function isEnding()
+    {
+        return !$this->connection->isWritable();
+    }
+
+    private function isWriting()
+    {
+        return $this->connection->getBuffer()->listening;
     }
 }
